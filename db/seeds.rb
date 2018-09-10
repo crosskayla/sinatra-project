7908b8c3a44a581436d91a69fc11795bd848585d
Kayla <kac288@georgetown.edu>
2018-09-09 22:54:34 -0400

    added Bootstrap

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Song.create(name: "Hello",
+  artist: "Adele",
+  difficulty: 5)
+
+Song.create(name: "The Ultracheese",
+    artist: "Arctic Monkeys",
+    difficulty: 4)
+
+Song.create(name: "Fly Me To The Moon",
+  artist: "Frank Sinatra",
+  difficulty: 7)
