81a382db0a1ab3312dd4a4c5de1145ac12d84397
Kayla <kac288@georgetown.edu>
2018-09-09 21:41:16 -0400

    initial commit

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,24 @@
+songclass SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    redirect to "songs/#{@song.id}"
+  end
+
+  get '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :'/songs/show'
+  end
+
+  patch '/songs/:id' do 
+    redirect to "songs/#{@song.id}"
+  end
+end
