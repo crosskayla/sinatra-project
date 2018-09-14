75ffe295e3e6438c6632d0e757b87e9ad15d8396
Kayla <kac288@georgetown.edu>
2018-09-14 15:40:53 -0400

    Delete functionality

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,8 +20,29 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    if @song.created_by == current_user.id
+      erb :'/songs/edit'
+    else
+      erb :'/failure'
+    end
+  end
+
   patch '/songs/:id' do
+    @song = Song.find(params[:id])
+    @song.update(params[:song])
     redirect to "/songs/#{@song.id}"
+  end
+
+  delete '/songs/:id' do
+    @song = Song.find(params[:id])
+    if @song.created_by == current_user.id
+      @song.destroy
+      redirect to "/songs"
+    else
+      erb :'/failure'
+    end
   end
 
 end
